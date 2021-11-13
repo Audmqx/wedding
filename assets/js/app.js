00dc4b91e2ca5f65277f06f47831e6178b49d1a2
Audmqx <iangaev.m@live.fr>
2021-11-13 23:48:17 +0100

    add section 2

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,14 +7,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     preLoaderAnimation.addEventListener("animationend", function(e) {
 
-		let preLoader = document.querySelector('.preloader')
-
-		// gsap.to(preLoader, {
-	 //                opacity: 0.,
-	 //                duration: 0.5
-	 //        })
-
+	let preLoader = document.querySelector('.preloader')
 	 preLoader.classList.add('header')
+
+	 gsap.to('.main-wrapper', {
+	                opacity: 1,
+	                duration: 0.5
+	        })
 
     }, false);
 });
@@ -51,6 +50,37 @@ var end = new Date('06/24/2022 2:00 PM');
     }
 
     timer = setInterval(showRemaining, 1000);
+
+//Animation Jas SVG
+
+// gsap.to("#jas_svg", {
+//   scrollTrigger: "#jas_svg", // start the animation when ".box" enters the viewport (once)
+//   x: 50
+// });
+
+ScrollTrigger.create({
+    trigger: '#section-2',
+    start: '80% bottom',
+    end: '20% top',
+    onEnter: (e) => {
+    	let jasSvg = document.querySelector('#jas_svg');
+    	jasSvg .classList.add('draw')
+    	setTimeout(function() {
+    		document.querySelector('.jas-img').style.opacity = "1";
+    			jasSvg .addEventListener("animationend", function() {
+    			gsap.to(jasSvg, {
+	                opacity: 0,
+	                duration: 0.5
+	        		})
+    		}, false);
+    	}, 1000);
+    }
+    // onUpdate: (self)=>{
+    //      console.log(self)
+    //      gsap.to("#jas_svg", {duration: 1, x: 100});
+    //         }
+        });
+
 
 
 // el.addEventListener("animationstart", function() {}, false);
