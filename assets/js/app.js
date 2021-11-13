ff532015c0456d1a7c9f85763be4997dea3899db
Audmqx <iangaev.m@live.fr>
2021-11-13 21:00:26 +0100

    add section

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,14 +17,42 @@ document.addEventListener('DOMContentLoaded', (event) => {
 	 preLoader.classList.add('header')
 
     }, false);
-
 });
+
+
+// Countdown
+var end = new Date('06/24/2022 2:00 PM');
+
+    var _second = 1000;
+    var _minute = _second * 60;
+    var _hour = _minute * 60;
+    var _day = _hour * 24;
+    var timer;
+
+    function showRemaining() {
+        var now = new Date();
+        var distance = end - now;
+        if (distance < 0) {
+
+            clearInterval(timer);
+            document.getElementById('countdown').innerHTML = 'EXPIRED!';
+
+            return;
+        }
+        var days = Math.floor(distance / _day);
+        var hours = Math.floor((distance % _day) / _hour);
+        var minutes = Math.floor((distance % _hour) / _minute);
+        var seconds = Math.floor((distance % _minute) / _second);
+
+        document.getElementById('countdown').innerHTML = days + 'j  ';
+        document.getElementById('countdown').innerHTML += hours + 'h  ';
+        document.getElementById('countdown').innerHTML += minutes + 'm  ';
+        document.getElementById('countdown').innerHTML += seconds + 's  ';
+    }
+
+    timer = setInterval(showRemaining, 1000);
 
 
 // el.addEventListener("animationstart", function() {}, false);
 // el.addEventListener("animationend", function() {}, false);
 // el.addEventListener("animationiteration", function() {}, false);
-
-
-// document.getElementById("svag").setAttribute("viewBox", y + " " + x + " 100 100");
-// window.onload to change the styles document.getElementbyId().style.display = none/inline
--- index.php
+++ index.php
@@ -11,11 +11,49 @@
 <body>
 	<!-- begin preloader -->
 	<div class="preloader">
+
 		<?php include 'views/components/logo.php'; ?> 
 	</div>
 	<!-- end preloader -->
-	<h1> salut </h1>
- 
+	
+ 	
+ 	<section>
+ 		<div class="content-wrapper">
+ 			<div class="porte-el">
+ 				<div class="content-porte">
+
+ 					<div class="save-the-date">
+ 						<div class="line-v"></div>
+ 						<p>24</p>
+ 						<p>JUIN</p>
+ 						<p>22</p>
+ 						<div class="line-v"></div>
+ 					</div>
+
+ 					<h1 class="script title-porte-el">Audrey et Maxim</h1>
+ 					<p>Vous invitent avec plaisir à les rejoindre pour célébrer leur mariage</p>
+ 					<p>Le Vendredi, 24 Juin 2020 à 16h00</p>
+ 					<h1 class="script">Le Jas du Pébrier</h1>
+ 					<p class="uppercase sans">1610 D8, 83520 Roquebrune-sur-Argens</p>
+
+ 				</div>
+ 			</div>
+ 		</div>
+ 	</section>
+ 	 	<section>
+ 		<h1> section 2 </h1>
+ 	</section>
+ 	 	<section>
+ 		<h1> section 3 </h1>
+ 	</section>
+ 	 	<section>
+ 		<h1> section 4 </h1>
+ 	</section>
+
+	<footer>
+		<div id="countdown"></div>
+	</footer>
+
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.8.0/gsap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.8.0/ScrollTrigger.min.js" integrity="sha512-CPA5LMoJI/a5HkSIAKcBtFXe4gqGjPUL2ExF/3PmhrrHI17wod9xOqqF+0WZQRKIIq0KwF8oG5BaiWobtrke3A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>	
